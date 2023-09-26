580229d08102804f7fda4782fe4ab8a1d49c5167
galaxy4276 <galaxyhi4276@gmail.com>
2023-09-27 01:25:27 +0900

    feat: 로거 생성 함수 작성

---
+++ src/service/logger.ts
@@ -0,0 +1,23 @@
+import winston, { Logger, LoggerOptions } from 'winston';
+import path from 'node:path';
+import { getNodeEnv, NodeEnvVariables } from "../config/variables";
+
+type ExtendedLoggerOptions = LoggerOptions & {
+  writeFile?: boolean;
+};
+
+/**
+ * @description Logger 객체를 생성합니다.
+ */
+export const createLogger = (name: string, option: ExtendedLoggerOptions): Logger => winston.createLogger({
+  defaultMeta: { name },
+  exitOnError: false,
+  transports: [
+    new winston.transports.Console(),
+    option.writeFile && new winston.transports.File({
+      dirname: path.resolve(__dirname, 'logs'),
+      filename: getNodeEnv() === NodeEnvVariables.DEV ? 'application.log' : 'error.log',
+    }),
+  ],
+  ...option,
+});
