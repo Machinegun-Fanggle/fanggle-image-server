d51e01f3ed7cbd0540ea1cc97cb5ee4dee2a0ef1
galaxy4276 <galaxyhi4276@gmail.com>
2023-09-27 01:17:25 +0900

    feat: 로그 디렉터리 생성 기능 작성

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
 import Fastify from 'fastify';
+import fs from 'node:fs/promises';
+import normalFs from 'node:fs';
 
 const server = Fastify({
   logger: true,
 });
 
+(async () => {
+  const exists = await new Promise(resolve =>
+    normalFs.access('./log', err => {
+      if (err) return resolve(false);
+      return resolve(true);
+    }));
+
+  if (!exists) await fs.mkdir('./log');
+})();
 
 server.listen({ path: 'localhost', port: 8880 }, (err, address) => {
   console.log(`Server Runned On ${address}`);
