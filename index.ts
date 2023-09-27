448691dbcb487f56bed47f4314b86e6f00e744c5
galaxy4276 <galaxyhi4276@gmail.com>
2023-09-27 13:38:54 +0900

    feat: s3 객체 작성

--- index.ts
+++ index.ts
@@ -1,9 +1,33 @@
 import Fastify from 'fastify';
 import fs from 'node:fs/promises';
 import normalFs from 'node:fs';
+import { createLogger } from './src/service/logger';
+import { S3Client } from "@aws-sdk/client-s3";
+import multer from 'fastify-multer';
+import multerS3 from 'multer-s3';
+import { config } from 'dotenv';
 
-const server = Fastify({
-  logger: true,
+config();
+
+const runLogger = createLogger('application-runner');
+
+const server = Fastify();
+
+server.register(multer.contentParser);
+
+const s3 = new S3Client();
+
+const uploader = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'machinegunsoft',
+    key: (req, file, cb) => {
+      cb(null, file.originalname + new Date().toISOString());
+    },
+  }) as any,
+  limits: {
+    fileSize: 100 * 1024 * 1024,
+  },
 });
 
 (async () => {
@@ -16,10 +40,19 @@ const server = Fastify({
   if (!exists) await fs.mkdir('./log');
 })();
 
+server.post('/upload', {
+  preHandler: uploader.single('file'),
+}, async (req, res) => {
+  return res.status(200).send();
+});
+
+
 server.listen({ path: 'localhost', port: 8880 }, (err, address) => {
-  console.log(`Server Runned On ${address}`);
+  runLogger.info(`Server Runned On ${address}`);
   if (err) {
-    console.error(err);
+    runLogger.error(`error when running application`);
+    runLogger.error(`error name is ${err.name}`);
+    runLogger.error(err.message);
     process.exit();
   }
 });
